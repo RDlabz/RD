cd4fb0703cc7a3ad030eab2914f3dc8e720e2d39
RD <rdl4bs@gmail.com>
2025-04-22 15:21:41 +0300

    layout and language

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+        {
+            source: '/',
+            destination: '/en/',
+            permanent: true,
+        },
+        {
+            source: '/:lang(en)',
+            destination: '/:lang/',
+            permanent: false,
+        },
+    ]
+  }
 };
 
 export default nextConfig;
