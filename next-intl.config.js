aafc27b19341e747405dfd55ddd13127223df210
RD <rdl4bs@gmail.com>
2025-04-27 02:38:55 +0300

    removed languages

--- next-intl.config.js
+++
@@ -1,6 +0,0 @@
-// next-intl.config.js
-module.exports = {
-    locales: ['en', 'gr'], // Add more as needed
-    defaultLocale: 'en'
-  };
-  
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "RDLabs",
+  description: "Slick, Efficient and Secured Applications",
+};
+
+export default async function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased rd-background`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
