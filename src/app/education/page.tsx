25840dd518a6463a10c869ce8dcff81121df5035
RD <rdl4bs@gmail.com>
2025-05-17 01:33:27 +0300

    Education content

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,9 +1,14 @@
 'use client'
+import en from "@/LabContent/en.json"
 
 export default function App() {
+
+  const edu = en.academics
   return (
     <main>
-      {"edu"}
+      {edu.uni.map((uni, ind) => (
+        <p key={"uni_"+ind} style={{textAlign: "justify", margin: "5px 0px", fontSize: !ind ? "var(--nav)" : "inherit"}}>{uni}</p>
+      ))}
     </main>
   );
 }
