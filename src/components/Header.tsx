b55f55f8f7ff02f2c0132f26eeabdba1d820b2f3
RD <rdl4bs@gmail.com>
2025-05-17 01:08:29 +0300

    router style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,12 +31,12 @@ export default function Header() {
     }, [])
 
     return (
-        <nav style={{width: "100dvw", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px 1dvw", backdropFilter: "brightness(70%) blur(30px)"}}>
+        <nav style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px 1dvw", backdropFilter: "brightness(70%) blur(30px)"}}>
             <p style={{fontSize: "var(--title)"}}>{title}<i style={{color: "var(--primary)"}}>{lastLetter}</i></p>
             <div style= {{display: "flex", columnGap: "1dvw"}}>
-                <p onClick={() => {router.push("/")}}>Home</p>
-                <p onClick={() => {router.push("/education")}}>Education</p>
-                <p onClick={() => {router.push("/projects")}}>Projects</p>
+                <a onClick={() => {router.push("/")}}>Home</a>
+                <a onClick={() => {router.push("/education")}}>Education</a>
+                <a onClick={() => {router.push("/projects")}}>Projects</a>
             </div>
         </nav>
     )
