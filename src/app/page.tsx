faf0dec22f156ae7a8b91677dd70238804749d58
RD <rdl4bs@gmail.com>
2025-05-17 01:30:43 +0300

    Home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 'use client'
+import en from "@/LabContent/en.json"
 
 export default function App() {
+
+  const home = en.home
+
   return (
     <main>
-      {"hello"}
+      <h2>{home.card.name}</h2>
+      <p style={{marginBottom: "5px"}}>{home.card.titles}</p>
+      <div style={{width: "100%", height: "1px", background: "var(--foreground)", margin: "5px 0px"}}></div>
+      {home.greeting.map((greet, ind) => (
+        <p key={"greeting_"+ind} style={{textAlign: "justify", margin: "5px 0px"}}>{greet}</p>
+      ))}
+      <small style={{marginTop: "50px"}}>This website is still under heavy constraction</small>
     </main>
   );
 }
