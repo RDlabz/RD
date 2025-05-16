1e00166f297f9363e62106c3f61d6e8760f67d2e
RD <rdl4bs@gmail.com>
2025-05-17 01:38:44 +0300

    Project content

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,8 @@
 export default function App() {
   return (
     <main>
-      {"project"}
+      <p>They are coming soon.</p>
+      <a href="https://github.com/Dr-Ardi?tab=repositories" target="_blank">For now, you can check them here.</a>
     </main>
   );
 }
