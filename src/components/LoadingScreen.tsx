608d2cce5b894c40333b5a28bf51ebab1d7974a8
RD <rdl4bs@gmail.com>
2025-07-27 15:30:34 +0300

    Added loader elements

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+export default function LoadingScreen() {
+    return (
+        <div style={{height: "100%", width: "100%", position: "absolute", zIndex: "999"}}>
+            <p>ELEGANT</p>
+            <p>EFFICIENT</p>
+            <p>SECURED</p>
+        </div>
+    )
+}
