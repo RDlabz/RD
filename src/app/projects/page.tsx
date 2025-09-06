dd2a99ef67560438dc735bf7989c7a3bd1cca358
RD <rdl4bs@gmail.com>
2025-09-06 17:55:22 +0300

    Project box design

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,26 +6,19 @@ import { useState } from 'react';
 export default function App() {
 
   const projects = en.projects
-  const [hoverredProject, setHoverredProject] = useState<string>("")
   const [selectedProject, setSelectedProject] = useState<any>({})
-  const [color, setColor] = useState('var(--foreground)');
 
   const selectProject = (project: any) => {
     setSelectedProject(project)
   }
 
-  const handleMouseLeave = () => {
-    setColor('var(--foreground)'); 
-  };
-
   return (
     <main>
       <h1>{projects.title}</h1>
-      <div className="flex flex-wrap gap-1 justify-evenly w-full">
+      <div className="flex flex-wrap gap-2 justify-evenly w-full py-5">
         {projects.projects.map((project) => (
           <ProjectCard 
             key={project.title} project={project} 
-            hoverredProject={hoverredProject} setHoverredProject={setHoverredProject} 
             selectProject={selectProject} />
         ))}
       </div>
@@ -33,9 +26,10 @@ export default function App() {
         selectedProject ? 
           <div>
             <a 
-              href={selectedProject.link} 
-              onMouseEnter={() => setColor(selectedProject.color)}
-              onMouseLeave={() => setColor('var(--foreground)')}
+              className="text-rd-sub"
+              href={selectedProject.link}
+              onMouseEnter={e => (e.currentTarget.style.color = selectedProject.color)}
+              onMouseLeave={e => (e.currentTarget.style.color = "var(--foreround)")} 
             >{selectedProject.title}</a>
             {selectedProject.description && selectedProject.description.map((desc: string, indx: number) => (
               <p key={"project-desc" + indx}>{desc}</p>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,27 +1,28 @@
-import '@/styles/ProjectCard.css'
+import {Icon} from "@iconify/react";
+import { useState } from "react";
 
 export default function ProjectCard({
     project,
-    hoverredProject,
-    setHoverredProject,
     selectProject
 }:{
     project: any,
-    hoverredProject: any,
-    setHoverredProject: any,
     selectProject: any
 }) {
 
+    const [hoverredProject, setHoverredProject] = useState<string>("")
+
     return (
-        <div className='card' onMouseEnter={() => setHoverredProject(project.title)} onMouseLeave={() => setHoverredProject("")}>
+        <div className='relative h-40 w-40 border-2 border-rd-white rounded-xl flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:w-80' onMouseEnter={() => setHoverredProject(project.title)} onMouseLeave={() => setHoverredProject("")}>
             {
                 (hoverredProject === project.title) ?
-                <div className='card-content' style={{backgroundColor: project.color}} onClick={() => selectProject(project)}>
-                    <p className='text-center text-rd-blue'>{project.abstract}</p>
+                <div className='absolute size-full rounded-xl flex flex-col justify-between items-start backdrop-blur-sm transition-all duration-300 ease-in-out text-rd-white px-2' style={{boxShadow: `inset 0 0px 70px -30px ${project.color}`}} onClick={() => selectProject(project)}>
+                    <p>{project.title}</p>
+                    <p className='text-rd-small'>{project.abstract}</p>
                 </div>
                 :
-                <h2 className='w-52 text-center'>{project.title}</h2>
+                null
             }
+            <Icon icon={`lucide:${project.icon}`} width={70} height={70} />
         </div>
     )
 }
