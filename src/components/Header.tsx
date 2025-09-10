f92db2500fec2abb4b47a125302b60baef38caa5
RD <rdl4bs@gmail.com>
2025-09-10 22:22:35 +0300

    Added nav panel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,13 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import {Icon} from "@iconify/react";
+import NavPanel from './NavPanel';
 
 export default function Header() {
 
     const [title, updateTitle] = useState("");
     const [lastLetter, setLastLetter] = useState("");
+    const [isOpen, toggleNav] = useState<boolean>(false);
 
     const websiteName = "RDLabs";
     let i = 0;
@@ -32,16 +34,20 @@ export default function Header() {
     }, [])
 
     return (
-        <nav className="sticky top-0 w-full h-16 flex justify-between items-center backdrop-brightness-75 backdrop-blur-[30px]">
-            <p className="text-rd-title font-rd-pixel cursor-pointer" title="Under Construction">{title}<span className="text-rd-primary">{lastLetter}</span></p>
-            <div id="navbar" className="gap-x-[1dvw] font-rd-terminal text-rd-sub">
-                <a onClick={() => {router.push("/.")}}>Home</a>
-                <a onClick={() => {router.push("/education")}}>Education</a>
-                <a onClick={() => {router.push("/projects")}}>Projects</a>
-            </div>
-            <button id="short-navbar">
-                <Icon icon="lucide:text-align-justify" width={32} height={32} />
-            </button>
-        </nav>
+        <>
+            <nav className="sticky top-0 w-full h-16 flex justify-between items-center backdrop-brightness-75 backdrop-blur-[30px]">
+                <p className="text-rd-title font-rd-pixel cursor-pointer" title="Under Construction">{title}<span className="text-rd-primary">{lastLetter}</span></p>
+                <div id="navbar" className="gap-x-[1dvw] font-rd-terminal text-rd-sub">
+                    <a onClick={() => {router.push("/.")}}>Home</a>
+                    <a onClick={() => {router.push("/education")}}>Education</a>
+                    <a onClick={() => {router.push("/projects")}}>Projects</a>
+                </div>
+                <button id="short-navbar" onClick={() => toggleNav(true)}>
+                    <Icon icon="lucide:text-align-justify" width={32} height={32} />
+                </button>
+            </nav>
+
+            <NavPanel isOpen={isOpen} toggle={toggleNav} />
+        </>
     )
 } 
---
+++ src/components/NavPanel.tsx
@@ -0,0 +1,36 @@
+"use client"
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation';
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function NavPanel({ 
+    isOpen,
+    toggle
+}: { 
+    isOpen: boolean
+    toggle: any
+}) {
+
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (isOpen) {
+        document.body.style.overflow = "hidden";
+        } else {
+        document.body.style.overflow = "";
+        }
+    }, [isOpen]);
+
+    if (!isOpen)
+        return null
+
+    return (
+        <div id="nav-panel" className="fixed inset-0 z-40 backdrop-blur-lg py-20 flex-col font-rd-terminal text-rd-nav justify-around items-center overflow-hidden">
+            <Icon className="absolute top-5 right-5 cursor-pointer z-50 p-1" icon={`lucide:x`} width={40} height={40} onClick={() => toggle(false)} />
+            <a className="w-1/2 text-center py-5" onClick={() => {router.push("/."); toggle(false)}}>Home</a>
+            <a className="w-1/2 text-center py-5" onClick={() => {router.push("/education"); toggle(false)}}>Education</a>
+            <a className="w-1/2 text-center py-5" onClick={() => {router.push("/projects"); toggle(false)}}>Projects</a>
+        </div>
+    );
+}
