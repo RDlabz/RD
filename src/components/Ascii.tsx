33de3af191d006325ac265b39d0eac091f8a7803
Dr-Ardi <ardi.str@proton.me>
2026-02-07 19:39:38 +0200

    Created temp ascii

---
+++ src/components/Ascii.tsx
@@ -0,0 +1,15 @@
+export default function Ascii() {
+    return (
+        <div className="w-full flex items-center justify-center">
+            <pre>{
+`.______          __________  
+|   _  \\        /  /       \\ 
+|  |_)  |      /  /|  .--.  |
+|      /      /  / |  |  |  |
+|  |\\  \\----./  /  |  '--'  |
+| _| \`._____/__/   |_______/ 
+`}
+            </pre>
+        </div>
+    )
+}
