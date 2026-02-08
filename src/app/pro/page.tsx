9ff63dbf5677713598fb8d4033f58a57e4daa0fc
Dr-Ardi <ardi.str@proton.me>
2026-02-08 16:21:57 +0200

    Projects

--- src/app/pro/page.tsx
+++ src/app/pro/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import en from '@/LabContent/en.json'
 import ProjectList from '@/components/ProjectList';
+import { BodyWindow, Window, WindowGrid } from '@/components/WindowManager';
+import { Icon } from "@iconify/react";
 
 const projects = en.projects
 
@@ -10,25 +12,32 @@ export default function App() {
   const [selectedProject, setSelectedProject] = useState<any>({})
 
   return (
-    <>
-      <h2>{projects.title}</h2>
-      <ProjectList projects={projects.projects} setSelectedProject={setSelectedProject} />
-      {
-        selectedProject ? 
-          <section id="projectDescription">
-            <a 
+    <WindowGrid>
+      <BodyWindow>
+        <Window title="Projects" padding={"12px 3%"}>
+          <p className="text-justify text-xl py-5">{projects.intro}</p>
+          <ProjectList projects={projects.projects} setSelectedProject={setSelectedProject} />
+        </Window>
+        {Boolean(Object.keys(selectedProject).length) && (
+          <Window title={selectedProject.abstract} padding={"10px 3%"}>
+            <div className="w-full sticky top-0 bg-rd-base z-10 h-8 flex items-center justify-end">
+              <Icon icon="material-symbols:close" width={30} height={30} className="cursor-pointer" onClick={() => { setSelectedProject({}) }} />
+            </div>
+            <a
+              id="projectDescription"
               href={selectedProject.link}
+              className="text-2xl font-bold"
               target="_blank"
               onMouseEnter={e => (e.currentTarget.style.color = selectedProject.color)}
-              onMouseLeave={e => (e.currentTarget.style.color = "var(--foreround)")} 
+              onMouseLeave={e => (e.currentTarget.style.color = "var(--foreround)")}
             >{selectedProject.title}</a>
             {selectedProject.description && selectedProject.description.map((desc: string, indx: number) => (
-              <p key={"project-desc" + indx} className="text-justify">{desc}</p>
+              <p key={"project-desc" + indx} className="text-justify text-xl">{desc}</p>
             ))}
-          </section>
-          :
-          null
-      }
-    </>
+            <div className='py-5'></div>
+          </Window>
+        )}
+      </BodyWindow>
+    </WindowGrid>
   );
 }
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useRef, useState, useEffect } from 'react';
 import ProjectCard from './ProjectCard';
 
 export default function ProjectList({
@@ -9,49 +8,16 @@ export default function ProjectList({
     projects: any[],
     setSelectedProject: any
 }) {
-    
-    const scrollRef = useRef<HTMLDivElement>(null);
-    const [showLeftFade, setShowLeftFade] = useState(false);
-    const [showRightFade, setShowRightFade] = useState(false);
-      
-    const updateFades = () => {
-        const container = scrollRef.current;
-        if (!container) return;
-    
-        const { scrollLeft, scrollWidth, clientWidth } = container;
-    
-        // Show left fade if not at the start
-        setShowLeftFade(scrollLeft > 20);
-    
-        // Show right fade if not at the end
-        setShowRightFade(scrollLeft + clientWidth < scrollWidth - 20);
-    };
-    
-    useEffect(() => {
-        updateFades(); // initial check
-    
-        const container = scrollRef.current;
-        if (!container) return;
-    
-        container.addEventListener('scroll', updateFades);
-        window.addEventListener('resize', updateFades);
-    
-        return () => {
-          container.removeEventListener('scroll', updateFades);
-          window.removeEventListener('resize', updateFades);
-        };
-    }, []);
 
     return (
-        <div className="relative overflow-hidden m-[-20px] py-10">
-            <div ref={scrollRef} className="x-list w-full px-[20px]">
-            {projects.map((project) => (
-                <ProjectCard key={project.title} project={project} selectProject={setSelectedProject} />
-            ))}
+        <div className="relative overflow-hidden py-10">
+            <div className="projectList gap-y-5 w-full">
+                {projects.map((project) => (
+                    <div key={project.title} className="flex justify-center">
+                        <ProjectCard project={project} selectProject={setSelectedProject} />
+                    </div>
+                ))}
             </div>
-
-            {showLeftFade && (<div className="absolute top-0 left-0 h-full w-3 pointer-events-none bg-gradient-to-r from-rd-primary to-transparent" />)}
-            {showRightFade && (<div className="absolute top-0 right-0 h-full w-3 pointer-events-none bg-gradient-to-l from-rd-primary to-transparent" />)}
         </div>
     )
 }
