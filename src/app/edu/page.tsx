bb709bdea70d133680e5ad6966d87ff1f5705a9f
Dr-Ardi <ardi.str@proton.me>
2026-02-08 15:26:32 +0200

    Education details

--- src/app/edu/page.tsx
+++ src/app/edu/page.tsx
@@ -9,7 +9,14 @@ export default function App() {
     <WindowGrid>
       <BodyWindow>
         <Window title="Education" padding={"12px 3%"}>
-          {edu.uni.map((uni, ind) => (<p key={"uni_"+ind} className="text-justify text-xl my-2">{uni}</p>))}
+          <div className="py-5">
+            {edu.uni.map((uni, ind) => {
+              if (uni.includes("**UNI**")) 
+                    return (<p className="text-justify text-xl my-2" key={"uni_"+ind}>{uni.replace("**UNI**", "")} <a className="font-extrabold" href={edu.university} target="_blank">IHU</a></p>)
+                  else
+                    return (<p className="text-justify text-xl my-2" key={"uni_"+ind}>{uni}</p>)
+            })}
+          </div>
         </Window>
       </BodyWindow>
     </WindowGrid>
